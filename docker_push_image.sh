177b789ef16f33460a2bfa566320f6a92fa7a644
mt <tinywritingmakesmailhardtoread@gmail.com>
2017-10-31 11:32:09 +0100

    fix travis with docker 2

--- docker_push_image.sh
+++ docker_push_image.sh
@@ -1,3 +1,3 @@
 docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
-docker build -t mrtrustworthy/docume_webapp:$TRAVIS_BUILD_NUMBER .
-docker push mrtrustworthy/docume_webapp:$TRAVIS_BUILD_NUMBER
+docker build -t "$DOCKER_USERNAME"/docume_webapp:$TRAVIS_BUILD_NUMBER .
+docker push "$DOCKER_USERNAME"/docume_webapp:$TRAVIS_BUILD_NUMBER
